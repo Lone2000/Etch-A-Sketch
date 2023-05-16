cdd4c55b847d55270ffc1ffdb7d816bd6afb511d
Lone <Lone0059@gmail.com>
2023-05-16 18:00:34 +0500

    Custom Grid
    This commit adds the input field and makes the grid system dynamic.

--- script.js
+++ script.js
@@ -18,16 +18,40 @@ let container = document.querySelector(".container");
 //     }
 // }
 
-function createDiv() {
+function createDiv(gridSize) {
+
+    // Clear previous grid
+    while (container.firstChild) {
+        container.firstChild.remove();
+      }
     
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+      
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
         let div = document.createElement('div');
         div.classList.add("box");
         div.textContent = "Grid";
         container.appendChild(div);
       }
     }
+
+    let containerWidth = gridSize * 40; // Can adjust width as needed;
+    container.style.containerWidth = `${containerWidth}px`
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
   }
 
-createDiv();
+
+// Grid Custom Size
+let gridSizeInput = document.querySelector("#gridSizeInput");
+
+gridSizeInput.addEventListener('input', handleGridSizeChange);
+
+
+function handleGridSizeChange(){
+    let gridSize = parseInt(gridSizeInput.value, 10);
+    createDiv(gridSize);
+
+    let containerWidth = gridSize * 40; // Can adjust width as needed;
+    container.style.containerWidth = `${containerWidth}px`
+}
